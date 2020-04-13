19064c133e0d32afc4bba549c0e9e7bc1da64469
axp <643382875@qq.com>
2020-04-13 18:07:05 +0800

    add: Post page

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,111 +4,115 @@ var toolJs = require("../../utils/tool.js");
 const _ = dbJs.db.command;
 
 Page({
-  data: {
-    currentType: 0,
-    currentSmallType: 0,
-    typeList: [],
-    smallTypeList: [],
-    dataList: [],
-  },
-  onLoad() {
-    dbJs.query("square_type").then((data) => {
-      this.setData({
-        typeList: data.data,
-      });
-      if (!this.data.smallTypeList.length) {
-        this.getEventList(data.data[0].id);
-      }
-    });
-  },
-  // onReady() {
-  //   console.log("onReady----");
-  //   if (!this.data.smallTypeList.length && this.data.typeList.length) {
-  //     this.getEventList(this.data.typeList[0].id);
-  //   }
-  // },
-  clickType(e) {
-    let id = e.currentTarget.dataset["id"],
-      idx = e.currentTarget.dataset["idx"];
-    console.log("~~~~~~id", id);
-    console.log("idx", idx);
-    if (this.data.currentType === idx) {
-      return false;
-    } else {
-      this.setData({
-        currentType: idx,
-      });
-      this.getEventList(id);
-    }
-  },
+    data: {
+        currentType: 0,
+        currentSmallType: 0,
+        typeList: [],
+        smallTypeList: [],
+        dataList: [],
+    },
+    onLoad() {
+        dbJs.query("square_type").then((data) => {
+            this.setData({
+                typeList: data.data,
+            });
+            if (!this.data.smallTypeList.length) {
+                this.getEventList(data.data[0].id);
+            }
+        });
+    },
+    // onReady() {
+    //   console.log("onReady----");
+    //   if (!this.data.smallTypeList.length && this.data.typeList.length) {
+    //     this.getEventList(this.data.typeList[0].id);
+    //   }
+    // },
+    clickType(e) {
+        let id = e.currentTarget.dataset["id"],
+            idx = e.currentTarget.dataset["idx"];
+        console.log("~~~~~~id", id);
+        console.log("idx", idx);
+        if (this.data.currentType === idx) {
+            return false;
+        } else {
+            this.setData({
+                currentType: idx,
+            });
+            this.getEventList(id);
+        }
+    },
 
-  clickSmallType(e) {
-    let id = e.currentTarget.dataset["id"],
-      idx = e.currentTarget.dataset["idx"];
+    clickSmallType(e) {
+        let id = e.currentTarget.dataset["id"],
+            idx = e.currentTarget.dataset["idx"];
 
-    if (this.data.currentSmallType === idx) {
-      return false;
-    } else {
-      this.setData({
-        currentSmallType: idx,
-      });
-      this.getDataList(id);
-    }
-  },
+        if (this.data.currentSmallType === idx) {
+            return false;
+        } else {
+            this.setData({
+                currentSmallType: idx,
+            });
+            this.getDataList(id);
+        }
+    },
 
-  getDataList(id) {
-    dbJs.query("square_data", { type_id: _.eq(id) }).then((data) => {
-      data.data.forEach((data) => {
-        data.release = toolJs.formatTime(data.release);
-        data.typeName = this.data.smallTypeList.find((st) => st.id == id).name;
-      });
-      this.setData({
-        dataList: data.data,
-      });
-    });
-  },
+    getDataList(id) {
+        dbJs.query("square_data", { type_id: _.eq(id) }).then((data) => {
+            data.data.forEach((data) => {
+                data.release = toolJs.formatTime(data.release);
+                data.typeName = this.data.smallTypeList.find(
+                    (st) => st.id == id
+                ).name;
+            });
+            this.setData({
+                dataList: data.data,
+            });
+        });
+    },
 
-  getEventList(id) {
-    dbJs.query("square_subtype", { type_id: _.eq(id) }).then((data) => {
-      this.setData({
-        smallTypeList: data.data,
-      });
-    });
-  },
+    getEventList(id) {
+        dbJs.query("square_subtype", { type_id: _.eq(id) }).then((data) => {
+            this.setData({
+                smallTypeList: data.data,
+            });
+        });
+    },
 
-  onTapdetail(event) {
-    let postItem = event.currentTarget.dataset.postitem;
-    console.log(postItem);
-    // url: "subpage/detail/detail?id=" + postItem,
-    wx.navigateTo({
-      url: "subpage/detail/detail?id=" + JSON.stringify(postItem),
-      complete: function (res) {
-        console.log("complete", res);
-        // 通过eventChannel向被打开页面传送数据
-        // res.eventChannel.emit("acceptDataFromOpenerPage", { data: "test" });
-      },
-    });
-  },
-  post() {
-    console.log('post')
+    onTapdetail(event) {
+        let postItem = event.currentTarget.dataset.postitem;
+        console.log(postItem);
+        // url: "subpage/detail/detail?id=" + postItem,
+        wx.navigateTo({
+            url: "subpage/detail/detail?id=" + JSON.stringify(postItem),
+            complete: function (res) {
+                console.log("complete", res);
+            },
+        });
+    },
+    post() {
+        wx.navigateTo({
+            url: "subpage/post/post",
+            complete: function (res) {
+                console.log("/post/post", res);
+            },
+        });
+    },
+    // getEventList(id) {
+    //   dbJs.query("square_subtype", { type_id: _.eq(id) }).then((data) => {
+    //     this.setData(
+    //       {
+    //         smallTypeList: data.data,
+    //       },
+    //       () => this.getRandomColor()
+    //     );
+    //   });
+    // },
+    // 0406
+    // getStyle(bg, isActive) {
+    //   console.log(`getStyle: ${bg}-${isActive}`)
+    //   return {
+    //     background: `#${bg}`
+    //   }
 
-  }
-  // getEventList(id) {
-  //   dbJs.query("square_subtype", { type_id: _.eq(id) }).then((data) => {
-  //     this.setData(
-  //       {
-  //         smallTypeList: data.data,
-  //       },
-  //       () => this.getRandomColor()
-  //     );
-  //   });
-  // },
-  // 0406
-  // getStyle(bg, isActive) {
-  //   console.log(`getStyle: ${bg}-${isActive}`)
-  //   return {
-  //     background: `#${bg}`
-  //   }
-
-  // }
+    // }
 });
