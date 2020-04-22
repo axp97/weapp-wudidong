0d22d635b397c5d3b7cafb1c049c28233eeee5aa
安小盼 <643382875@qq.com>
2020-04-22 22:27:48 +0800

    add: 点赞功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,248 +5,242 @@ const _ = dbJs.db.command;
 const app = getApp();
 
 Page({
-    data: {
-        currentType: undefined,
-        currentSmallType: undefined,
-        typeList: [],
-        smallTypeList: [],
-        dataList: [],
-        showModal: false,
-        subTypeName: undefined,
-        subTypeColor: undefined,
-    },
-    // onshow  onLoad
-    onLoad() {
-        dbJs.query("square_type").then((res) => {
-            let data = res.data,
-                dataLen = data.length;
-            this.setData({
-                typeList: data,
-            });
-            this.setData({
-                currentType: dataLen ? 0 : undefined,
-            });
-            if (!dataLen) return;
-            this.getSubTypeList(data[0]._id);
+  data: {
+    currentType: undefined,
+    currentSmallType: undefined,
+    typeList: [],
+    smallTypeList: [],
+    dataList: [],
+    showModal: false,
+    subTypeName: undefined,
+    subTypeColor: undefined,
+    zanList: [],
+  },
+  // onshow  onLoad
+  onLoad() {
+    dbJs.query("square_type").then((res) => {
+      let data = res.data,
+        dataLen = data.length;
+      this.setData({
+        typeList: data,
+      });
+      this.setData({
+        currentType: dataLen ? 0 : undefined,
+      });
+      if (!dataLen) return;
+      this.getSubTypeList(data[0]._id);
+    });
+    this.getZanList();
+  },
+
+  onShow() {
+    let smTypeIdx = this.data.currentSmallType;
+    if (smTypeIdx == undefined) return;
+    let id = this.data.smallTypeList[smTypeIdx]._id;
+    this.getList(id);
+  },
+
+  getSubTypeList(_id) {
+    dbJs.query("square_subtype", { type_id: _.eq(_id) }).then((res) => {
+      let data = res.data,
+        dataLen = data.length;
+      this.setData({
+        smallTypeList: data,
+      });
+      this.setData({
+        currentSmallType: dataLen ? 0 : undefined,
+      });
+      if (!dataLen) return;
+      this.getList(data[0]._id);
+    });
+  },
+
+  getList(id) {
+    dbJs.query("square_data", { type_id: _.eq(id) }).then((res) => {
+      let data = res.data;
+      if (!data.length) {
+        this.setData({
+          dataList: [],
         });
-      this.getZanList();
-    },
-
-    onShow() {
-        let smTypeIdx = this.data.currentSmallType;
-        if (smTypeIdx == undefined) return;
-        let id = this.data.smallTypeList[smTypeIdx]._id;
-        this.getList(id);
-    },
-
-    getSubTypeList(_id) {
-        dbJs.query("square_subtype", { type_id: _.eq(_id) }).then((res) => {
-            let data = res.data,
-                dataLen = data.length;
-            this.setData({
-                smallTypeList: data,
-            });
-            this.setData({
-                currentSmallType: dataLen ? 0 : undefined,
-            });
-            if (!dataLen) return;
-            this.getList(data[0]._id);
-        });
-    },
-
-    getList(id) {
-        dbJs.query("square_data", { type_id: _.eq(id) }).then((res) => {
-            let data = res.data;
-            if (!data.length) {
-                this.setData({
-                    dataList: [],
-                });
-                return;
-            }
-            // this.queryZan();
-            data.forEach((item) => {
-                item.release = toolJs.formatTime(item.release);
-                item.typeName = this.data.smallTypeList.find(
-                    (st) => st._id == id
-                ).name;
-            });
-            this.setData({
-                dataList: data,
-            });
-        });
-    },
-
-    clickType(e) {
-        let id = e.currentTarget.dataset["id"],
-            idx = e.currentTarget.dataset["idx"];
-        if (this.data.currentType === idx) {
-            return false;
+        return;
+      }
+      data.forEach((item, index) => {
+        item.release = toolJs.formatTime(item.release);
+        item.typeName = this.data.smallTypeList.find((st) => st._id == id).name;
+        if (!this.data.zanList.length) {
+          item.isLike = false;
+          // this.setData({
+          //   [this.data.dataList[index].isLike]: false,
+          // });
+          return;
         }
-        this.setData({
-            currentType: idx,
-        });
-        this.getSubTypeList(id);
-    },
-
-    clickSmallType(e) {
-        let id = e.currentTarget.dataset["id"],
-            idx = e.currentTarget.dataset["idx"];
-
-        if (this.data.currentSmallType === idx) {
-            return false;
-        } else {
-            this.setData({
-                currentSmallType: idx,
-            });
-            this.getList(id);
+        let hasStatus = this.data.zanList.find(
+          (zan) => zan.post_id == item._id
+        );
+        if (!hasStatus) {
+          item.isLike = false;
+          // this.setData({
+          //   [this.data.dataList[index].isLike]: false,
+          // });
+          return;
         }
-    },
+        item.isLike = !!hasStatus.status;
+        // this.setData({
+        //   [this.data.dataList[index].isLike]: !!hasStatus.status,
+        // });
+      });
+      this.setData({
+        dataList: data,
+      });
+    });
+  },
 
-    toDetailPage(event) {
-        let postItem = event.currentTarget.dataset.postitem;
-        wx.navigateTo({
-            url: "subpage/detail/detail?id=" + JSON.stringify(postItem),
-            complete: function (res) {},
-        });
-    },
+  clickType(e) {
+    let id = e.currentTarget.dataset["id"],
+      idx = e.currentTarget.dataset["idx"];
+    if (this.data.currentType === idx) {
+      return false;
+    }
+    this.setData({
+      currentType: idx,
+    });
+    this.getSubTypeList(id);
+  },
 
-    post() {
-        wx.navigateTo({
-            url:
-                "subpage/post/post?id=" +
-                JSON.stringify(this.data.smallTypeList),
-            complete: function (res) {},
-        });
-    },
+  clickSmallType(e) {
+    let id = e.currentTarget.dataset["id"],
+      idx = e.currentTarget.dataset["idx"];
 
-    async zan(e) {
-        let post_id = e.currentTarget.dataset.postid,
-            user_id = wx.getStorageSync("openid") || (await app.getOpenid());
-        dbJs.db
-            .collection("square_data")
-            .doc(post_id)
-            .update({
-                data: {
-                    like_num: _.inc(1),
-                },
-            });
-        let param = {
-            post_id,
-            status: 1,
-            type: 1,
-            user_id,
-        };
-        dbJs.add("zan", param).then((res) => {});
-    },
+    if (this.data.currentSmallType === idx) {
+      return false;
+    } else {
+      this.setData({
+        currentSmallType: idx,
+      });
+      this.getList(id);
+    }
+  },
 
-    /**
-  
-     * 获取收藏和喜欢的状态
-  
-     */
+  toDetailPage(event) {
+    let postItem = event.currentTarget.dataset.postitem;
+    wx.navigateTo({
+      url: "subpage/detail/detail?id=" + JSON.stringify(postItem),
+      complete: function (res) {},
+    });
+  },
 
-    // getPostRelated: async function (blogId) {
-    //     let where = {
-    //         postId: blogId,
+  post() {
+    wx.navigateTo({
+      url: "subpage/post/post?id=" + JSON.stringify(this.data.smallTypeList),
+      complete: function (res) {},
+    });
+  },
 
-    //         openId: app.globalData.openid,
-    //     };
+  async zan(e) {
+    let post_id = e.currentTarget.dataset.postid,
+      curStatus = e.currentTarget.dataset.status,
+      index = e.currentTarget.dataset.index,
+      user_id = wx.getStorageSync("openid") || (await app.getOpenid());
+    dbJs.db
+      .collection("square_data")
+      .doc(post_id)
+      .update({
+        data: {
+          like_num: _.inc(curStatus ? -1 : 1),
+        },
+      });
+    let likeNum = this.data.dataList[index].like_num;
+    this.setData({
+      [`dataList[${index}].isLike`]: !curStatus,
+      [`dataList[${index}].like_num`]: curStatus ? --likeNum : ++likeNum,
+    });
+    let param = {
+      post_id,
+      status: curStatus ? 0 : 1,
+      type: 1,
+      user_id,
+    };
 
-    //     let postRelated = await api.getPostRelated(where, 1);
+    if (!this.data.zanList.length) {
+      dbJs.add("zan", param).then((res) => {
+        this.getZanList();
+      });
+      return;
+    }
 
-    //     let that = this;
+    let hasStatus = this.data.zanList.find((zan) => zan.post_id == post_id);
+    if (!hasStatus) {
+      dbJs.add("zan", param).then((res) => {
+        this.getZanList();
+      });
+      return;
+    }
+    let that = this;
+    dbJs.db
+      .collection("zan")
+      .doc(hasStatus._id)
+      .update({
+        data: {
+          status: curStatus ? 0 : 1,
+        },
+        complete(res) {
+          that.getZanList();
+        },
+      });
+  },
 
-    //     for (var item of postRelated.data) {
-    //         if (config.postRelatedType.COLLECTION === item.type) {
-    //             that.setData({
-    //                 collection: {
-    //                     status: true,
-    //                     text: "已收藏",
-    //                     icon: "favorfill",
-    //                 },
-    //             });
+  async getZanList() {
+    let user_id = wx.getStorageSync("openid") || (await app.getOpenid());
+    dbJs.query("zan", { user_id }).then((res) => {
+      this.setData({
+        zanList: res.data,
+      });
+    });
+  },
 
-    //             continue;
-    //         }
+  // modal
+  addSubType() {
+    this.setData({
+      showModal: true,
+    });
+  },
 
-    //         if (config.postRelatedType.ZAN === item.type) {
-    //             that.setData({
-    //                 zan: { status: true, text: "已赞", icon: "appreciatefill" },
-    //             });
+  modalConfirm() {
+    let name = this.data.subTypeName,
+      bg = this.data.subTypeColor;
+    if (!name) return;
+    let type_id = this.data.typeList[this.data.currentType]._id,
+      param = {
+        type_id,
+        name,
+        bg,
+      };
+    dbJs.add("square_subtype", param).then((res) => {
+      this.getSubTypeList(type_id);
+    });
+    this.setData({
+      showModal: false,
+    });
+  },
 
-    //             continue;
-    //         }
-    //     }
-    // },
+  saveSubTypeName(e) {
+    this.setData({
+      subTypeName: e.detail.value,
+    });
+  },
 
-  getZanList() {
-    let postId = wx.getStorageSync("openid") || (await app.getOpenid());
-    dbJs.query("zan", { post_id: _.eq(postId) }).then((res) => {
-      console.log('getZanList', res)
-              // let data = res.data;
-              // if (!data.length) {
-              //     this.setData({
-              //         dataList: [],
-              //     });
-              //     return;
-              // }
-            
-              // this.queryZan();
-              // data.forEach((item) => {
-              //     item.release = toolJs.formatTime(item.release);
-              //     item.typeName = this.data.smallTypeList.find(
-              //         (st) => st._id == id
-              //     ).name;
-              // });
-              // this.setData({
-              //     dataList: data,
-              // });
-          });
-      },
+  saveSubTypeColor(e) {
+    this.setData({
+      subTypeColor: e.detail.value,
+    });
+  },
 
-    // modal
-    addSubType() {
-        this.setData({
-            showModal: true,
-        });
-    },
-
-    modalConfirm() {
-        let name = this.data.subTypeName,
-            bg = this.data.subTypeColor;
-        if (!name) return;
-        let type_id = this.data.typeList[this.data.currentType]._id,
-            param = {
-                type_id,
-                name,
-                bg,
-            };
-        dbJs.add("square_subtype", param).then((res) => {
-            this.getSubTypeList(type_id);
-        });
-        this.setData({
-            showModal: false,
-        });
-    },
-
-    saveSubTypeName(e) {
-        this.setData({
-            subTypeName: e.detail.value,
-        });
-    },
-
-    saveSubTypeColor(e) {
-        this.setData({
-            subTypeColor: e.detail.value,
-        });
-    },
-
-    previewImg(e) {
-        let current = e.currentTarget.dataset.url,
-            urls = e.currentTarget.dataset.urls;
-        wx.previewImage({
-            current,
-            urls,
-        });
-    },
+  previewImg(e) {
+    let current = e.currentTarget.dataset.url,
+      urls = e.currentTarget.dataset.urls;
+    wx.previewImage({
+      current,
+      urls,
+    });
+  },
 });
--- pages/index/subpage/detail/detail.js
+++ pages/index/subpage/detail/detail.js
@@ -1,7 +1,13 @@
+var dbJs = require("../../../../utils/db");
+const app = getApp();
 Page({
   data: {
     detail: undefined,
+    releaseFocus: false,
+    commentList: [],
+    // content: undefined
   },
+
   onLoad: function (option) {
     let obj = JSON.parse(option.id);
     console.log("postItem", obj);
@@ -9,12 +15,29 @@ Page({
       detail: obj,
     });
   },
+
   previewImg(e) {
     let current = e.currentTarget.dataset.url,
       urls = e.currentTarget.dataset.urls;
     wx.previewImage({
       current,
       urls,
+    });
+  },
+
+  bindReply: function (e) {
+    this.setData({
+      releaseFocus: true,
+    });
+  },
+  // comment
+  async getZanList() {
+    let user_id = wx.getStorageSync("openid") || (await app.getOpenid()),
+      post_id = this.data.detail._id;
+    dbJs.query("comment", { user_id, post_id }).then((res) => {
+      this.setData({
+        commentList: res.data,
+      });
     });
   },
 });
